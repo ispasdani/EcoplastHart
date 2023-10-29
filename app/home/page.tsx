f2ec6b7e19cbfa6d63c3a419a6ada5e547b026bd
ispasdani <ispasdani14@gmail.com>
2023-10-29 08:36:46 +0100

    test

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,12 @@
 import Nav from "@/components/Nav/Nav";
+import Heropage from "@/pages/Heropage";
 import React from "react";
 
 const Homepage = () => {
   return (
     <div className="text-color">
       <Nav />
-      Home page
+      <Heropage />
     </div>
   );
 };
